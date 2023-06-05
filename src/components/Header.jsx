4cd82c1ef7299e5dec0e22bcb64f8d0ec5c7deff
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-06-05 17:35:37 -0300

    feat - criação dos componentes Section e Footer e implementação no Home

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Pokedexpédia</h1>
+        <input type="search" name="" id="" />
+        <button type="submit">Pesquisar</button>
+      </header>
+    )
+  }
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Section from '../components/Section';
+import Footer from '../components/Footer';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Section />
+        <Footer />
+      </div>
+    )
+  }
+  
+}
+
+export default Home;
